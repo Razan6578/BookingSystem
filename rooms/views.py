027db86333f8f550cd980e045c92082816cf9beb
Razan6578 <ngvozdik11@gmail.com>
2026-01-10 14:17:25 +0200

    created 3 models

---
+++ rooms/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Room, Booking
+
+# Create your views here.
+
+# Функція представлення списку всіх кімнат
+def rooms_list(request):
+    rooms = Room.objects.all()
+    context = {
+        "rooms": rooms
+    }
+    return render(request=request, template_name="room_list.html", context=context)
